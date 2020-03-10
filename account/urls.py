34f4599ac9ee44d0b87c83f3f03a99756ddb441c
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-03-11 00:21:26 +0900

    usernameでdetailページを開けるように設定

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import SignUp,RegistProfileView
+from .views import SignUp,RegistProfileView,ProfileDetailView
 
 app_name='account'
 urlpatterns = [
     path('signup/',SignUp.as_view(),name='signup'),
     path('signup/registprofile/',RegistProfileView.as_view(),name='registprofile'),
+    path('detail/<str:username>/',ProfileDetailView.as_view(),name='profiledetail'),
 ]
