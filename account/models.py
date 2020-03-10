a30bc7d950a8c415902edc3b8f2c7675f483212e
Yuki Tamaribuchi <uk.tmbc2408@gmail.com>
2020-03-10 19:06:24 +0900

    Profileモデルの追加

--- account/models.py
+++ account/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    handle = models.CharField(max_length=20)
+    location = models.CharField(max_length=20)
+    biograph = models.CharField(max_length=200)
+
+    def __str__(self):
+        return user
