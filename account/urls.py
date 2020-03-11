2ad9131b64fc7d6ad41ac4f31060a9342a7d0668
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-03-11 10:57:03 +0900

    ログアウトのルーティング

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import SignUp,RegistProfileView,ProfileDetailView,LoginView
+from django.contrib.auth import views as auth_views
 
 app_name='account'
 urlpatterns = [
@@ -7,4 +8,5 @@ urlpatterns = [
     path('signup/registprofile/',RegistProfileView.as_view(),name='registprofile'),
     path('detail/<str:username>/',ProfileDetailView.as_view(),name='profiledetail'),
     path('login/',LoginView.as_view(),name='login'),
+    path('logout/',auth_views.LogoutView.as_view(),name='logout')
 ]
