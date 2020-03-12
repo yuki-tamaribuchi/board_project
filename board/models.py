8e8972bee74a50b4ac85b0b63ceb183c07350f5c
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-03-12 10:17:56 +0900

    Topicモデルの追加

--- board/models.py
+++ board/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from account.models import Profile
 # Create your models here.
+class Topic(models.Model):
+    content=models.CharField(max_length=200)
+    user=models.ForeignKey(Profile,on_delete=models.CASCADE)
+    date_time=models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return "%s/%s" % (self.content,self.user)
