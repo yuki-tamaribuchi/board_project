dfc21cd9cb56c7a5e372cebb8a37aac123e94220
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-03-12 11:30:29 +0900

    indexlistのルーティング

--- board/urls.py
+++ board/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView,IndexListView
 
 app_name='board'
 urlpatterns = [
-    path('',IndexView.as_view(),name='index')
+    path('',IndexView.as_view(),name='index'),
+    path('indexlist/',IndexListView.as_view(),name='indexlist'),
 ]
