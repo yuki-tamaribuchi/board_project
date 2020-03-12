24ca2e12238e23da68afe5b8f8bf640cb9727b8f
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-03-12 11:30:46 +0900

    IndexListViewの追加

--- board/views.py
+++ board/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,ListView
+from .models import Topic
 
 # Create your views here.
 class IndexView(TemplateView):
     template_name="board/index.html"
+
+
+class IndexListView(ListView):
+    model=Topic
+    context_object_name='topics'
+    template_name='board/indexlist.html'
