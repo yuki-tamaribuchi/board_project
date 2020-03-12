c49e3209b1297e280a655c1f2f4f8549359961e4
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-03-12 16:33:05 +0900

    TopicCreateViewの追加

--- board/views.py
+++ board/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render,get_object_or_404
-from django.views.generic import TemplateView,ListView,DetailView
+from django.views.generic import TemplateView,ListView,DetailView,CreateView
 from .models import Topic
-
+from django.urls import reverse_lazy
+from .forms import TopicCreateForm
+from django.http import HttpResponseRedirect
+from account.models import Profile
 
 # Create your views here.
 class IndexView(TemplateView):
@@ -18,3 +21,17 @@ class TopicDetailView(DetailView):
     model=Topic
     template_name='board/detail.html'
     context_oject_name='topic'
+
+
+class TopicCreateView(CreateView):
+    form_class=TopicCreateForm
+    template_name='board/topiccreate.html'
+    
+    def post(self,request):
+        form=TopicCreateForm(request.POST)
+
+        new_topic=form.save(commit=False)
+        new_topic.user=Profile.objects.get(id=request.user.id)
+        new_topic.save()
+        url=reverse_lazy('board:indexlist')
+        return HttpResponseRedirect(url)
