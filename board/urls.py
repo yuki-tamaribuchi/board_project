810325567baa96308cbc4c82215502e45f4e7d70
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-03-12 13:41:08 +0900

    トピック詳細のルーティング

--- board/urls.py
+++ board/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import IndexView,IndexListView
+from .views import IndexView,IndexListView,TopicDetailView
 
 app_name='board'
 urlpatterns = [
     path('',IndexView.as_view(),name='index'),
     path('indexlist/',IndexListView.as_view(),name='indexlist'),
+    path('detail/<int:pk>/',TopicDetailView.as_view(),name='detail'),
 ]
