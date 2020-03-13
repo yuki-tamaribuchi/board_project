e2082c15ba25f3df42cd0e0cb7ff765cea38ee20
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-03-13 10:33:09 +0900

    Followモデルの作成

--- account/models.py
+++ account/models.py
@@ -10,3 +10,11 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class Follow(models.Model):
+    following_user=models.ForeignKey(Profile,on_delete=models.CASCADE,related_name='following_user')
+    followed_user=models.ForeignKey(Profile,on_delete=models.CASCADE,related_name='followed_user')
+
+    def __str__(self):
+        return '%s is following %s' % (self.following_user,self.followed_user)
