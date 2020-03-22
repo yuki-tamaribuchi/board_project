45740ff5776ebd54b0af8980e58ab54a1fde011b
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-03-22 13:44:19 +0900

    リプライページのルーティング

--- board/urls.py
+++ board/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import IndexView,IndexListView,TopicDetailView,TopicCreateView,TopicDeleteView
+from .views import IndexView,IndexListView,TopicDetailView,TopicCreateView,TopicDeleteView,ReplyCreateView
 
 app_name='board'
 urlpatterns = [
@@ -7,5 +7,6 @@ urlpatterns = [
     path('indexlist/',IndexListView.as_view(),name='indexlist'),
     path('detail/<int:pk>/',TopicDetailView.as_view(),name='detail'),
     path('createtopic/',TopicCreateView.as_view(),name='create'),
+    path('reply/<int:pk>/',ReplyCreateView.as_view(),name='reply'),
     path('delete/<int:pk>/',TopicDeleteView.as_view(),name='delete'),
 ]
